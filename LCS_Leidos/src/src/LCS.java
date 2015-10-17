1c271c7060baa7cce9a457ff575945cc6a94115d
aratikaushik <arati.kaushik@gmail.com>
2015-10-16 19:59:22 -0500

    LCS Leidos comments changes

--- LCS_Leidos/src/src/LCS.java
+++ LCS_Leidos/src/src/LCS.java
@@ -4,6 +4,6 @@ import java.util.Vector;
 
 public class LCS {
 	public static void lcs(Vector<Integer> a, Vector<Integer> b, Vector<Integer> res){
-		//@find lcs of two integer vectors
+		//@ Find the longest common sequence between two vectors of integers
 	}
 }
