c2b969309c1d8afbd43fbe3d978d84f8cdd7f564
aratikaushik <arati.kaushik@gmail.com>
2015-10-20 09:07:59 -0500

    Correct test cases

--- Bresenham_Leidos/src/test/TestBresenham.java
+++ Bresenham_Leidos/src/test/TestBresenham.java
@@ -3,19 +3,16 @@ package test;
 import src.Pair;
 import src.Bresenham;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-import java.util.HashSet;
-import java.util.Set;
 import java.util.Vector;
 
 public class TestBresenham {
 
 	static Vector<Pair> res;
-	
+
 	@Before
 	public void setup() {
 		res = new Vector<Pair>();
@@ -24,8 +21,8 @@ public class TestBresenham {
 	@Test
 	public void test1() {
 		Vector<Pair> v1 = new Vector<Pair>();
-		v1.add(new Pair(1, 0));
 		v1.add(new Pair(0, 0));
+		v1.add(new Pair(1, 0));
 		Bresenham.drawLine(1, 0, res);
 		assertEquals(v1, res);
 	}
@@ -33,9 +30,9 @@ public class TestBresenham {
 	@Test
 	public void test2() {
 		Vector<Pair> v1 = new Vector<Pair>();
-		v1.add(new Pair(1, 2));
-		v1.add(new Pair(0, 1));
 		v1.add(new Pair(0, 0));
+		v1.add(new Pair(1, 1));
+		v1.add(new Pair(1, 2));
 		Bresenham.drawLine(1, 2, res);
 		assertEquals(v1, res);
 	}
@@ -43,10 +40,10 @@ public class TestBresenham {
 	@Test
 	public void test3() {
 		Vector<Pair> v1 = new Vector<Pair>();
-		v1.add(new Pair(3, 0));
-		v1.add(new Pair(2, 0));
-		v1.add(new Pair(1, 0));
 		v1.add(new Pair(0, 0));
+		v1.add(new Pair(1, 0));
+		v1.add(new Pair(2, 0));
+		v1.add(new Pair(3, 0));
 		Bresenham.drawLine(3, 0, res);
 		assertEquals(v1, res);
 	}
@@ -54,12 +51,12 @@ public class TestBresenham {
 	@Test
 	public void test4() {
 		Vector<Pair> v1 = new Vector<Pair>();
-		v1.add(new Pair(5, 3));
-		v1.add(new Pair(4, 2));
-		v1.add(new Pair(3, 2));
-		v1.add(new Pair(2, 1));
-		v1.add(new Pair(1, 1));
 		v1.add(new Pair(0, 0));
+		v1.add(new Pair(1, 1));
+		v1.add(new Pair(2, 1));
+		v1.add(new Pair(3, 2));
+		v1.add(new Pair(4, 2));
+		v1.add(new Pair(5, 3));
 		Bresenham.drawLine(5, 3, res);
 		assertEquals(v1, res);
 	}
@@ -67,12 +64,12 @@ public class TestBresenham {
 	@Test
 	public void test5() {
 		Vector<Pair> v1 = new Vector<Pair>();
-		v1.add(new Pair(3, 5));
-		v1.add(new Pair(2, 4));
-		v1.add(new Pair(2, 3));
-		v1.add(new Pair(1, 2));
-		v1.add(new Pair(1, 1));
 		v1.add(new Pair(0, 0));
+		v1.add(new Pair(1, 1));
+		v1.add(new Pair(1, 2));
+		v1.add(new Pair(2, 3));
+		v1.add(new Pair(2, 4));
+		v1.add(new Pair(3, 5));
 		Bresenham.drawLine(3, 5, res);
 		assertEquals(v1, res);
 	}
